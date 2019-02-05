128b0f580696c01336efa32d9b708929a246ae47
Aravind KR <kraravind1993@gmail.com>
2019-02-05 21:37:00 +0530

    updated userEntity class

--- quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
+++ quora-service/src/main/java/com/upgrad/quora/service/entity/UserEntity.java
@@ -14,7 +14,7 @@ public class UserEntity implements Serializable {
     @Id
     @Column(name = "id")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Integer user_id;
 
     @Column(name = "uuid")
     @NotNull
@@ -74,12 +74,12 @@ public class UserEntity implements Serializable {
         return new EqualsBuilder().append(this, obj).isEquals();
     }
 
-    public Integer getId() {
-        return id;
+    public Integer getUser_id() {
+        return user_id;
     }
 
-    public void setId(Integer id) {
-        this.id = id;
+    public void setUser_id(Integer user_id) {
+        this.user_id = user_id;
     }
 
     public String getUuid() {
